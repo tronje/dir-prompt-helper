045236df4b3f10d068304da7bc8fc58229b0ee9a
Tronje Krabbe <hi@tron.je>
2022-03-07 11:35:28 +0100

    Adapt to newest Rust version
    This includes minor changes (switching from into_iter to iter and a
    rustfmt run), and an auto-generated change in Cargo.lock.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,12 @@
 use std::process::Command;
 
-
 fn whoami() -> String {
     let output = Command::new("whoami")
         .output()
         .expect("Failed to run 'whoami'!");
 
-    String::from_utf8(output.stdout)
-        .unwrap()
-        .trim()
-        .to_owned()
+    String::from_utf8(output.stdout).unwrap().trim().to_owned()
 }
-
 
 fn pwd() -> String {
     let output = Command::new("pwd")
@@ -19,12 +14,8 @@ fn pwd() -> String {
         .output()
         .expect("Failed to run 'pwd'!");
 
-    String::from_utf8(output.stdout)
-        .unwrap()
-        .trim()
-        .to_owned()
+    String::from_utf8(output.stdout).unwrap().trim().to_owned()
 }
-
 
 fn build_string() -> String {
     let mut pwd = pwd();
@@ -35,20 +26,27 @@ fn build_string() -> String {
         pwd = pwd.replace(&home, "~");
     }
 
-    pwd = pwd.split('/').map(|dir| {
-        if pwd.ends_with(dir) {
-            String::from("/") + dir
-
-        } else if dir.starts_with(".") {
-            ['/', dir.chars().nth(0).unwrap(), dir.chars().nth(1).unwrap()]
-                .into_iter().collect::<String>()
-        } else {
-            match dir.chars().nth(0) {
-                Some(c) => ['/', c].into_iter().collect::<String>(),
-                None => String::from(""),
+    pwd = pwd
+        .split('/')
+        .map(|dir| {
+            if pwd.ends_with(dir) {
+                String::from("/") + dir
+            } else if dir.starts_with(".") {
+                [
+                    '/',
+                    dir.chars().nth(0).unwrap(),
+                    dir.chars().nth(1).unwrap(),
+                ]
+                .iter()
+                .collect::<String>()
+            } else {
+                match dir.chars().nth(0) {
+                    Some(c) => ['/', c].iter().collect::<String>(),
+                    None => String::from(""),
+                }
             }
-        }
-    }).collect::<String>();
+        })
+        .collect::<String>();
 
     if pwd.starts_with("/~") {
         pwd = pwd.replace("/~", "~");
@@ -56,7 +54,6 @@ fn build_string() -> String {
 
     pwd.replace("//", "/")
 }
-
 
 fn main() {
     println!("{}", build_string());
