48423b8dca86cdfaf6a45bf5a5fca9c1bbe9bcee
Tronje Krabbe <hi@tron.je>
2023-04-03 17:12:17 +0200

    Rework algorithm to avoid allocations
    The only allocations that happen now are, as far as I can tell,
    unavoidable. They happen in std::env::var("HOME") and
    std::env::current_dir(), both of which return a heap-allocated object.
    
    Other allocations are avoided by just printing results directly to
    stdout rather than allocating a String and printing it whole.

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,131 @@
-use std::process::Command;
+use std::env;
+use std::io::{self, Write};
+use std::path::{Path, MAIN_SEPARATOR_STR};
 
-fn whoami() -> String {
-    let output = Command::new("whoami")
-        .output()
-        .expect("Failed to run 'whoami'!");
+// Catch-all error to avoid allocations in Box<dyn Error>
+#[derive(Debug)]
+struct StaticError;
 
-    String::from_utf8(output.stdout).unwrap().trim().to_owned()
+impl<E: std::error::Error> From<E> for StaticError {
+    fn from(_: E) -> Self {
+        Self
+    }
 }
 
-fn pwd() -> String {
-    let output = Command::new("pwd")
-        .arg("-L")
-        .output()
-        .expect("Failed to run 'pwd'!");
+type Result<T> = std::result::Result<T, StaticError>;
 
-    String::from_utf8(output.stdout).unwrap().trim().to_owned()
-}
-
-fn shorten(path: String) -> String {
-    let mut output = String::with_capacity(path.len());
-
-    let home = format!("/home/{}", whoami());
+fn print_short_pwd<W>(pwd: &Path, home: &str, out: &mut W) -> Result<()>
+where
+    W: Write,
+{
+    let final_idx = pwd.iter().count() - 1;
 
     let mut skip = 0;
 
-    if path.starts_with(&home) {
-        output.push_str("~");
-        // skip 3 components and not only 2, because the first one is the empty string
+    if pwd.starts_with(home) {
+        out.write_all(b"~")?;
+        if final_idx >= 3 {
+            out.write_all(b"/")?;
+        }
+
+        // skip /home/<username>, which is three path components (root dir, home, and <username>)
         skip = 3;
     }
 
-    let last = path.split('/').count() - 1;
-    for (index, component) in path.split('/').enumerate() {
-        if index < skip || component.len() == 0 {
-            continue;
-        }
+    for (idx, component) in pwd.iter().enumerate().skip(skip) {
+        let s = component.to_str().unwrap();
 
-        output.push('/');
-        if index == last {
-            output.push_str(component);
+        if idx == final_idx {
+            out.write_all(s.as_bytes())?;
         } else {
-            if component.starts_with(".") {
-                output.push('.');
-                output.push(component.chars().nth(1).unwrap());
+            if s == MAIN_SEPARATOR_STR {
+                // do nothing
+            } else if s.starts_with('.') {
+                out.write_all(s[0..2].as_bytes())?;
             } else {
-                output.push(component.chars().nth(0).unwrap());
+                out.write_all(s[0..1].as_bytes())?;
             }
+
+            out.write_all(b"/")?;
         }
     }
 
-    output
+    out.write_all(b"\n")?;
+    out.flush()?;
+    Ok(())
+}
+
+fn run() -> Result<()> {
+    let pwd = env::current_dir()?;
+    let home = env::var("HOME")?;
+    let mut stdout = io::stdout().lock();
+    print_short_pwd(&pwd, &home, &mut stdout)?;
+    Ok(())
 }
 
 fn main() {
-    println!("{}", shorten(pwd()));
+    match run() {
+        Ok(_) => {}
+        Err(_) => println!("???"),
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::path::PathBuf;
+
+    fn test_helper(path: &Path, home: &str) -> String {
+        let mut out = Vec::new();
+        print_short_pwd(path, home, &mut out).unwrap();
+        String::from_utf8(out).unwrap()
+    }
+
+    #[test]
+    fn root() {
+        let path = PathBuf::from("/");
+        let home = "/home/alice";
+        let result = test_helper(&path, home);
+        assert_eq!(&result, "/\n");
+    }
 
     #[test]
     fn home() {
-        let path = format!("/home/{}", whoami());
-        assert_eq!(shorten(path), "~".to_owned());
+        let path = PathBuf::from("/home/alice");
+        let home = "/home/alice";
+        let result = test_helper(&path, home);
+        assert_eq!(&result, "~\n");
     }
 
     #[test]
-    fn some_path() {
-        let path = String::from("/alpha/bravo/charlie/delta/echo/foxtrott");
-        assert_eq!(shorten(path), "/a/b/c/d/e/foxtrott".to_owned());
+    fn foreign_home() {
+        let path = PathBuf::from("/home/bob");
+        let home = "/home/alice";
+        let result = test_helper(&path, home);
+        assert_eq!(&result, "/h/bob\n");
     }
 
     #[test]
-    fn somehwere_in_home() {
-        let path = format!("/home/{}/alpha/bravo/charlie", whoami());
-        assert_eq!(shorten(path), "~/a/b/charlie".to_owned());
+    fn somewhere_outside_home() {
+        let path = PathBuf::from("/alpha/bravo/charlie/delta/echo/foxtrott");
+        let home = "/home/doesnt_matter";
+        let result = test_helper(&path, home);
+        assert_eq!(&result, "/a/b/c/d/e/foxtrott\n");
+    }
+
+    #[test]
+    fn somewhere_in_home() {
+        let path = PathBuf::from("/home/alice/alpha/bravo/charlie");
+        let home = "/home/alice";
+        let result = test_helper(&path, home);
+        assert_eq!(&result, "~/a/b/charlie\n");
     }
 
     #[test]
     fn hidden_components() {
-        let path = String::from("/alpha/.bravo/charlie/.delta/echo");
-        assert_eq!(shorten(path), "/a/.b/c/.d/echo".to_owned());
+        let path = PathBuf::from("/alpha/.bravo/charlie/.delta/echo");
+        let home = "/home/doesnt_matter";
+        let result = test_helper(&path, home);
+        assert_eq!(&result, "/a/.b/c/.d/echo\n");
     }
 }
